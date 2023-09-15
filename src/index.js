046888dd2057b403ebd7028499d192f4ceee7986
João Pedro <joaopjt@icloud.com>
2023-09-14 22:34:55 -0300

    added e() method value

--- src/index.js
+++ src/index.js
@@ -260,7 +260,7 @@ class Tent {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['hand'],
       web: []
     };
     
@@ -276,19 +276,19 @@ class Tent {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['wood'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['big'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['heart'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['gold'];
           break;
       }
     }
